82c26ee5153919dff63c9348eb09f1c01bb1dbb5
Korguv <82498591+Korguv@users.noreply.github.com>
2022-03-01 12:52:47 +0300

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+
+def generate_alphanum_random_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    rand_string = ''.join(random.sample(letters_and_digits, length))
+    print("Alphanum Random string of length", length, "is:", rand_string)
+
+
+generate_alphanum_random_string(20)
