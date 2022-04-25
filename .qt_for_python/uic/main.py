4f8f670c0837f4fe34c0a16c126457c425731024
Korguv <82498591+Korguv@users.noreply.github.com>
2022-04-25 16:56:38 +0300

    new_naim

--- .qt_for_python/uic/main.py
+++ .qt_for_python/uic/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\Users\Admin_CPU\Desktop\Сode\1\Poliur\w_base.ui'
+# Form implementation generated from reading ui file 'c:\Users\Admin_CPU\Desktop\Сode\1\Poliur\main.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.6
 #
---
+++ test_1.py
@@ -0,0 +1,7 @@
+n= ["x",1,2,3]
+m= ['v',1,2,3]
+print(n)
+if n[1:4]==m[1:4]:
+    print ("Win")
+else:
+    print("Fail")
