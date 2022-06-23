b7f44a8739463388eedf4c562f00d3c45bd765c4
Korguv <82498591+Korguv@users.noreply.github.com>
2022-06-23 17:15:37 +0300

    +

--- .qt_for_python/uic/main.py
+++ .qt_for_python/uic/main.py
@@ -2129,7 +2129,7 @@ class Ui_w_main(object):
         self.menubar.addAction(self.menu_2.menuAction())
 
         self.retranslateUi(w_main)
-        self.tab_all.setCurrentIndex(0)
+        self.tab_all.setCurrentIndex(9)
         QtCore.QMetaObject.connectSlotsByName(w_main)
 
     def retranslateUi(self, w_main):
